7fee639bcef9b11740579e10165be7baedba945a
Edinson Enrique Pineda Salgado <epinedas@SAVIASALUDEPS.LOCAL>
2026-07-30 11:22:27 -0500

    Fix MainActivity package path ClassNotFoundException crash on launch

--- android/app/src/main/kotlin/com/example/waypulse_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.waypulse_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/waypulse/waypulse_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.waypulse.waypulse_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
