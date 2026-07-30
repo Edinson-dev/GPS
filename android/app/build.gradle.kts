885e5c9b1126c32bf325712a3f3dec7d4fe627b7
Edinson Enrique Pineda Salgado <epinedas@SAVIASALUDEPS.LOCAL>
2026-07-30 10:37:01 -0500

    Remove package attribute from AndroidManifest.xml for AGP 8 compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.waypulse_app"
+    namespace = "com.waypulse.waypulse_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -15,8 +15,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.waypulse_app"
+        applicationId = "com.waypulse.waypulse_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
